86ca413565350c449fae31c4c1c98ec3bdd04a3d
ari <arifaiwu@gmail.com>
2022-04-07 14:08:50 -0300

    Add question

--- Assets/Igualesono.cs
+++ Assets/Igualesono.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
 public class Igualesono : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     public int num1;
     public int num2;
     void Start()
